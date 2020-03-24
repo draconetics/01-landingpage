434ffbf00f6d6493b1735ae8581972e74419275e
mario <marioflorescondori@gmail.com>
2020-03-24 17:14:29 -0400

    making documentation

--- src/server.js
+++ src/server.js
@@ -3,13 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const morgan = require('morgan');
+//documentation
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 //database
 const db = require('./config/db.js');
-
 
 //midellware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));     
 
 //routes
 const emailsRoutes = require('./routes/emails');
